2f4bc550965b78fec3500022e19a50d86f1c2b8a
조성현 <nextday848@gmail.com>
2025-08-06 13:52:58 +0900

    🎉 Initial commit: spike generator + testbench + input data generator

--- rtl/01_input_spike_index.v
+++ rtl/01_input_spike_index.v
@@ -1,18 +1,19 @@
 module spike_generator (
   input wire clk,
   input wire reset,
-  input wire [63:0] spike_in
+  input wire [63:0] spike_in,
+  input wire [63:0] inhib_flag
 );
 
-  reg [3:0] time_step;
+  reg [9:0] time_step;
 
   always @(posedge clk or posedge reset) begin
     if (reset) begin
       time_step <= 0;
     end else begin
       time_step <= time_step + 1;
-      // 현재 spike 값 출력 (디버깅용)
-      $display("Time: %0t, Spike In = %b", $time, spike_in);
+      // 디버깅 출력
+      $display("Time: %0t, Spike In = %b, Inhibition Flag = %b", $time, spike_in, inhib_flag);
     end
   end
 
